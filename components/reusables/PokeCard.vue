c8e71b59102dedcff4166473acc228e2c0a0b443
Muhammad Azka <hello.mazka@gmail.com>
2024-01-23 11:18:21 +0700

    perf: added some codes and improvements

--- components/reusables/PokeCard.vue
+++ components/reusables/PokeCard.vue
@@ -1,3 +1,12 @@
+<script setup lang="ts">
+defineProps({
+  name: String,
+  imageUrl: String,
+  id: String,
+})
+</script>
 <template>
-  poke card
+  <div class="border-neutral-300 rounded-lg border flex flex-col h-32 items-center justify-center capitalize">
+    {{ name }}
+  </div>
 </template>
--- pages/index.vue
+++ pages/index.vue
@@ -1,18 +1,41 @@
 <script setup lang="ts">
 import { ref } from 'vue';
+import PokeCard from '~/components/reusables/PokeCard.vue';
 
-const { data, error } = await useFetch("https://pokeapi.co/api/v2/pokemon");
+type fetchResultType = {
+  name: string
+  url: string
+}
 
-const results = ref((data.value as any).results);
+const currentData = ref(0)
+const pokemons = ref<fetchResultType[]>([])
+const loading = ref(false)
+
+const fetchData = async () => {
+  currentData.value += 30
+  loading.value = true
+  try {
+    const { data } = await useFetch("https://pokeapi.co/api/v2/pokemon?limit=" + currentData.value +"&offset=0");
+    pokemons.value = [...pokemons.value, ...(data.value as any).results]
+  } catch (error) {
+    console.log(error)
+  }
+  loading.value = false
+}
+
+fetchData()
 </script>
 
 <template>
-  <div class="my-4 mx-6 space-y-8">
+  <div class="my-8 mx-6 space-y-8">
     <h1 class="text-4xl font-semibold text-center">Pokemons</h1>
-    <div class="grid grid-cols-12 gap-4">
-      <div v-for="poke in results">
-        <ReusablesPokeCard class="col-span-3" />
+    <div class="grid grid-cols-12 gap-3">
+      <div v-for="poke in pokemons" class="col-span-1">
+        <PokeCard :name="poke.name" />
       </div>
+    </div>
+    <div class="flex justify-center items-center">
+      <button :disabled="loading" @click="fetchData" class="btn btn-primary">{{ loading ? 'Loading...' : 'Load More' }}</button>
     </div>
   </div>
 </template>
