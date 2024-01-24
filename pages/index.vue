787146b0d7b38138c5019e999ea87b562e951afe
Muhammad Azka <hello.mazka@gmail.com>
2024-01-24 15:59:48 +0700

    fix: load more fetching

--- pages/index.vue
+++ pages/index.vue
@@ -8,32 +8,30 @@ type fetchResultType = {
 };
 
 const currentData = ref(30);
-const pokemons = ref<fetchResultType[]>([]);
-const loading = ref(false);
-const { data: { value }, pending } = await useFetch(
+const { data, pending } = await useFetch(
   "https://pokeapi.co/api/v2/pokemon", {
     query: {
-      limit: currentData.value,
+      limit: currentData,
       offset: 0,
     },
     lazy: true,
     watch: [currentData],
   }
 );
-console.log(value)
 </script>
 
 <template>
   <div class="my-8 mx-6 space-y-8">
     <h1 class="text-4xl font-semibold text-center">Pokemons</h1>
     <div class="grid grid-cols-12 gap-3">
-      <!-- <div v-for="poke in results" class="col-span-1"> -->
-        <!-- <PokeCard :name="poke.name" /> -->
+      <div v-for="poke in ((data as any).results as fetchResultType[])" class="col-span-1">
+        <PokeCard :name="poke.name" />
       </div>
     </div>
     <div class="flex justify-center items-center">
-      <button @click="currentData+=30" :disabled="pending" class="btn btn-primary">
-        {{ pending ? "Loading..." : "Load More" }}
+      <button @click="currentData += 30" :disabled="pending" class="btn btn-primary">
+        {{ pending ? "Loading" : "Load more" }}
+        <span v-if="pending" class="loading loading-ring"></span>
       </button>
     </div>
   </div>
